8b4453f9d957a97a8a20ff1753a94a5724b39281
Vipul Chaskar <chaskar.vipul@gmail.com>
2017-03-29 21:47:18 +0530

    Added requirements.txt

--- config/mongo_config.py
+++ config/mongo_config.py
@@ -1,4 +1,5 @@
 from flask_pymongo import PyMongo
+import os
 
 MONGO_DBNAME = 'classadmin'
 MONGO_URI = 'mongodb://localhost:27017/classadmin'
@@ -11,8 +12,12 @@ mongo = None
 def setup_mongo_connection(app):
 	global mongo
 
+	MONGO_URL = os.environ.get('MONGO_URL')
+	if not MONGO_URL:
+    	MONGO_URL = MONGO_URI;
+
 	app.config['MONGO_DBNAME'] = MONGO_DBNAME
-	app.config['MONGO_URI'] = MONGO_URI
+	app.config['MONGO_URI'] = MONGO_URL
 	app.config['MONGO_USERNAME'] = MONGO_USERNAME
 	app.config['MONGO_PASSWORD'] = MONGO_PASSWORD
 
